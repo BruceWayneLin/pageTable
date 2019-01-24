074b50b3af18881d5d59c184a0f3c305d1a6bd94
Skanda lin <anikiwayne@gmail.com>
2019-01-24 11:56:33 +0800

    updated router

--- table-page/src/router.js
+++ table-page/src/router.js
@@ -5,7 +5,7 @@ import maintain from './views/maintain.vue'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
+  mode: 'hash',
   // base: process.env.BASE_URL,
   routes: [{
       path: '/',
--- table-page/src/views/Home.vue
+++ table-page/src/views/Home.vue
@@ -19,18 +19,17 @@ export default {
   },
   methods: {},
   mounted() {
-    console.log(this.$route.query.p1);
-
+    "http://localhost:8081/#/?p1=0&p2=0&p3=48&p4=fjsapfe8wpuevjaur539woeitroiewlvlsnvdwaur9813up9HY%28%2aY%28HPNfhdoahfe923uwhofhe12y34321yrhehefpfjdsajfh09e2urjpfp";
     var agent_id = this.$route.query.p1;
     var game_id = this.$route.query.p2;
     var user_id = this.$route.query.p3;
     var payload = this.$route.query.p4;
+    window.location.pathName = this.$router.push({ path: "/", replace: true });
     var data = {};
-    data["agent_id"] = 0;
-    data["game_id"] = 0;
-    data["user_id"] = 48;
-    data["payload"] =
-      "fjsapfe8wpuevjaur539woeitroiewlvlsnvdwaur9813up9HY(*Y(HPNfhdoahfe923uwhofhe12y34321yrhehefpfjdsajfh09e2urjpfp";
+    data["agent_id"] = agent_id;
+    data["game_id"] = game_id;
+    data["user_id"] = user_id;
+    data["payload"] = payload;
     data["url"] = "https://stage.ucoin.club:30001/api/history";
     this.$store.dispatch("callApi", data);
   }
