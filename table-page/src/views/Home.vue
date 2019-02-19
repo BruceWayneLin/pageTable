aabad83715fb3c095abe165979b97c0fad159c8c
Skanda lin <anikiwayne@gmail.com>
2019-02-19 12:09:33 +0800

    no message

--- table-page/src/views/Home.vue
+++ table-page/src/views/Home.vue
@@ -22,13 +22,13 @@ export default {
     // http://localhost:8081/#/?p1=0&p2=0&p3=48&p4=fjsapfe8wpuevjaur539woeitroiewlvlsnvdwaur9813up9HY%28%2aY%28HPNfhdoahfe923uwhofhe12y34321yrhehefpfjdsajfh09e2urjpfp
     var agent_id = this.$route.query.p1;
     var game_id = this.$route.query.p2;
-    var user_id = this.$route.query.p3;
+    var token = this.$route.query.p3;
     var payload = this.$route.query.p4;
     // window.location.pathName = this.$router.push({ path: "/", replace: true });
     var data = {};
     data["agent_id"] = agent_id;
     data["game_id"] = game_id;
-    data["user_id"] = user_id;
+    data["token"] = token;
     data["payload"] = payload;
     data["url"] = "https://stage.ucoin.club:30001/api/history";
     this.$store.dispatch("callApi", data);
